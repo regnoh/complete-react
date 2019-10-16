a53aa1224fbb18ea9b7e672581f989fd19b3db8a
lixiaoru <lixiaoru@sensetime.com>
2019-10-16 19:13:38 +0800

    antd Table

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,6 +1,48 @@
 import React from "react";
+import { Card, Button, Table } from "antd";
+const dataSource = [
+  {
+    key: "1",
+    name: "胡彦斌",
+    age: 32,
+    address: "西湖区湖底公园1号"
+  },
+  {
+    key: "2",
+    name: "胡彦祖",
+    age: 42,
+    address: "西湖区湖底公园1号"
+  }
+];
+
+const columns = [
+  {
+    title: "姓名",
+    dataIndex: "name",
+    key: "name"
+  },
+  {
+    title: "年龄",
+    dataIndex: "age",
+    key: "age"
+  },
+  {
+    title: "住址",
+    dataIndex: "address",
+    key: "address"
+  }
+];
+
 const ArticleList = () => {
-  return <div>文章列表</div>;
+  return (
+    <Card
+      title="文章列表"
+      extra={<Button>导出为excel</Button>}
+      bordered={false}
+    >
+      <Table dataSource={dataSource} columns={columns} />
+    </Card>
+  );
 };
 
 export default ArticleList;
