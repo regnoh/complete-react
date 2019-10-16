31526b588c65a5404e2c67f1aeec79565d0d89f5
lixiaoru <lixiaoru@sensetime.com>
2019-10-16 15:00:18 +0800

    react-loadable:懒加载

--- src/tests/index.js
+++ src/tests/index.js
@@ -1,3 +1,4 @@
 export { default as TestHoc } from "./TestHoc";
 export { default as HocWrappedComp } from "./HocWrappedComp";
 export { default as TestAntd } from "./TestAntd";
+export { default as TestLoadable } from "./TestLoadable";
