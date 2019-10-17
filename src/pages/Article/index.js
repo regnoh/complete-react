67ebdb8753bf54e33ef0cae0bc8c1e6abcd891a3
lixiaoru <lixiaoru@sensetime.com>
2019-10-17 10:06:10 +0800

    1 根据columnKey设置Table column title 為中文 2 rowKey： datasource: [{ key: id 也可以写在<Table rowKey={record=>record.id} 3 时间戳 ->为年份（createAt） 4 render + Tag: 根据amount大小渲不同顔色的antd Tag

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,20 +1,46 @@
 import React, { useEffect, useState } from "react";
-import { Card, Button, Table } from "antd";
+import { Card, Button, Table, Tag } from "antd";
 import { fetchArticles } from "../../services/articles";
 const ArticleList = () => {
   const [dataSource, setDataSource] = useState([]);
   const [columns, setColumns] = useState([]);
   const [total, setTotal] = useState(0);
+  const ARTICLE_COLUMN_ZH_MAP = {
+    title: "标题",
+    author: "作者",
+    createAt: "时间",
+    amount: "阅读量",
+    action: "操作"
+  };
+  const getColumTitle = key => {
+    // 根据某种类别进行条件渲染, 可独立定义map
+    return ARTICLE_COLUMN_ZH_MAP[key];
+  };
   const getColumns = list => {
     const keys = Object.keys(list[0]).filter(key => key !== "id");
     const columns = [
-      ...keys.map(key => ({
-        title: key,
-        name: key,
-        dataIndex: key
-      })),
+      ...keys.map(key => {
+        if (key === "amount") {
+          return {
+            title: getColumTitle(key),
+            name: key,
+            dataIndex: key,
+            render: (text, record) => {
+              // 根据阅读量大小进行条件渲染
+              const color = record.amount > 400 ? "red" : "green";
+              return <Tag color={color}>{record.amount}</Tag>;
+            }
+          };
+        } else {
+          return {
+            title: getColumTitle(key),
+            name: key,
+            dataIndex: key
+          };
+        }
+      }),
       {
-        title: "action",
+        title: "操作",
         dataIndex: "action",
         key: "action",
         render: (text, record, index) => {
@@ -25,12 +51,10 @@ const ArticleList = () => {
     return columns;
   };
   const getDataSource = list => {
-    return list.map(({ id, title, author, createAt, amount }) => ({
-      key: id,
-      title,
-      author,
-      createAt: new Date(createAt).getFullYear(),
-      amount
+    return list.map(item => ({
+      key: item.id,
+      ...item,
+      createAt: new Date(item.createAt).getFullYear()
     }));
   };
 
@@ -49,6 +73,7 @@ const ArticleList = () => {
       bordered={false}
     >
       <Table
+        // rowKey={record => record.id}
         loading={dataSource.length === 0}
         dataSource={dataSource}
         columns={columns}
