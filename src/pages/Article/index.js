e13f97130ec39fd7128df8dfb269c272c49786e2
lixiaoru <lixiaoru@sensetime.com>
2019-10-17 11:42:59 +0800

    <Table loading={loading}

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -3,18 +3,19 @@ import { Card, Button, Table, Tag } from "antd";
 import dayjs from "dayjs";
 // import moment from "moment";// 235.4k
 import { fetchArticles } from "../../services/articles";
+import ButtonGroup from "antd/lib/button/button-group";
 // 将dayjs挂载到window上，便于在浏览器console直接使用dayjs
 // window.dayjs = dayjs;
 const ArticleList = () => {
   const [dataSource, setDataSource] = useState([]);
   const [columns, setColumns] = useState([]);
   const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState(false);
   const ARTICLE_COLUMN_ZH_MAP = {
     title: "标题",
     author: "作者",
     createAt: "时间",
-    amount: "阅读量",
-    action: "操作"
+    amount: "阅读量"
   };
   const getColumTitle = key => {
     // 根据某种类别进行条件渲染, 可独立定义map
@@ -48,7 +49,16 @@ const ArticleList = () => {
         dataIndex: "action",
         key: "action",
         render: (text, record, index) => {
-          return <Button>编辑</Button>;
+          return (
+            <ButtonGroup>
+              <Button size="small" type="primary">
+                编辑
+              </Button>
+              <Button size="small" type="danger">
+                删除
+              </Button>
+            </ButtonGroup>
+          );
         }
       }
     ];
@@ -64,12 +74,18 @@ const ArticleList = () => {
   };
 
   useEffect(() => {
-    fetchArticles().then(res => {
-      // console.log("res ", res);
-      setTotal(res.total);
-      setDataSource(getDataSource(res.list));
-      setColumns(getColumns(res.list));
-    });
+    setLoading(true);
+    fetchArticles()
+      .then(res => {
+        // console.log("res ", res);
+        setTotal(res.total);
+        setDataSource(getDataSource(res.list));
+        setColumns(getColumns(res.list));
+      })
+      .catch(err => {}) // services/articles.js全局处理过了
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
   return (
     <Card
@@ -79,7 +95,7 @@ const ArticleList = () => {
     >
       <Table
         // rowKey={record => record.id}
-        loading={dataSource.length === 0}
+        loading={loading}
         dataSource={dataSource}
         columns={columns}
         pagination={{
