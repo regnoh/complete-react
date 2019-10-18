029ba6c4f44919f45effea89e802a119597420e2
lixiaoru <lixiaoru@sensetime.com>
2019-10-18 09:31:52 +0800

    添加DataPicker，返回数据为Moment, 待转化为时间戳

--- src/pages/Article/Edit.js
+++ src/pages/Article/Edit.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Card, Button, Form, Input } from "antd";
+import { Card, Button, Form, Input, DatePicker } from "antd";
 const Edit = ({ form }) => {
   // const toBack = () => {};
   const formItemLayout = {
@@ -29,7 +29,9 @@ const Edit = ({ form }) => {
     e.preventDefault();
     form.validateFields((err, values) => {
       if (!err) {
-        // console.log("Received values of form: ", values);
+        console.log("Received values of form: ", values);
+        // FIXME: DataPicker 返回时间需要格式化为时间戳
+        // {title："哈哈", author: "刘阿达", createAt: Moment}
       }
     });
   };
@@ -46,8 +48,28 @@ const Edit = ({ form }) => {
       <Form {...formItemLayout} onSubmit={handleSubmit}>
         <Form.Item label="标题">
           {form.getFieldDecorator("title", {
-            rules: [{ required: true, message: "请输入文章标题!" }]
+            rules: [
+              { required: true, message: "请输入文章标题!" },
+              { min: 2, message: "最少2位!" },
+              { max: 20, message: "最多20位!" }
+            ]
           })(<Input placeholder="标题" />)}
+        </Form.Item>
+        <Form.Item label="作者">
+          {form.getFieldDecorator("author", {
+            rules: [{ required: true, message: "请输入文章作者!" }]
+          })(<Input placeholder="Mike" />)}
+        </Form.Item>
+        <Form.Item label="发布时间">
+          {form.getFieldDecorator("createAt", {
+            rules: [
+              {
+                type: "object",
+                required: true,
+                message: "请选择发布时间!"
+              }
+            ]
+          })(<DatePicker showTime />)}
         </Form.Item>
         <Form.Item {...tailFormItemLayout}>
           <Button type="primary" htmlType="submit">
@@ -60,4 +82,4 @@ const Edit = ({ form }) => {
 };
 // export default Edit;
 // 获取props.form
-export default Form.create()(Edit);
+export default Form.create({})(Edit);
