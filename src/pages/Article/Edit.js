61d33f778cb324fd58cb88bd4ee93b6b3194fc0b
lixiaoru <lixiaoru@sensetime.com>
2019-10-18 10:04:03 +0800

    1. 默认值:  便于测试 eg: 使用moment对DatePicker设置initValue: moment(new Date()) 2.InputNumber: 阅读量

--- src/pages/Article/Edit.js
+++ src/pages/Article/Edit.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Card, Button, Form, Input, DatePicker } from "antd";
+import { Card, Button, Form, Input, DatePicker, InputNumber } from "antd";
+import moment from "moment";
 const Edit = ({ form }) => {
   // const toBack = () => {};
   const formItemLayout = {
@@ -52,6 +53,7 @@ const Edit = ({ form }) => {
       <Form {...formItemLayout} onSubmit={handleSubmit}>
         <Form.Item label="标题">
           {form.getFieldDecorator("title", {
+            initialValue: "《父母》",
             rules: [
               { required: true, message: "请输入文章标题!" },
               { min: 2, message: "最少2位!" },
@@ -61,11 +63,20 @@ const Edit = ({ form }) => {
         </Form.Item>
         <Form.Item label="作者">
           {form.getFieldDecorator("author", {
+            initialValue: "麦克",
             rules: [{ required: true, message: "请输入文章作者!" }]
           })(<Input placeholder="Mike" />)}
         </Form.Item>
+        <Form.Item label="阅读量">
+          {form.getFieldDecorator("amount", {
+            initialValue: 0,
+            rules: [{ required: true, message: "请输入文章阅读量!" }]
+          })(<InputNumber min={0} />)}
+          <span className="ant-form-text"> 次</span>
+        </Form.Item>
         <Form.Item label="发布时间">
           {form.getFieldDecorator("createAt", {
+            initialValue: moment(new Date()),
             rules: [
               {
                 type: "object",
